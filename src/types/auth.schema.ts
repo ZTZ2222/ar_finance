0d281060f012918e5ebbbe291d85f168553c5c61
ZTZ <tzamirov@gmail.com>
2024-07-25 09:55:24 +0600

    create user, credentials schemas and types

--- src/types/auth.schema.ts
+++ src/types/auth.schema.ts
@@ -1,8 +1,8 @@
 import { z } from "zod"
 
 export const credentialsSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(1),
+  email: z.string().email({ message: "Некорректная почта" }),
+  password: z.string().min(1, { message: "Обязательное поле" }),
 })
 
 export type zCredentials = z.infer<typeof credentialsSchema>
