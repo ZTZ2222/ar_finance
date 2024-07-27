8f3ed2fb0fdb0c844eefbda5ddc26c9925678a7e
ZTZ <tzamirov@gmail.com>
2024-07-27 21:55:25 +0600

    create back-to-top-button + float-whatsapp

---
+++ src/components/layout/float-whatsapp.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+import Link from "next/link"
+import { WhatsApp } from "@/components/icons"
+
+export default function FloatWhatsApp() {
+  return (
+    <div className="fixed bottom-4 right-4 z-50">
+      <Link
+        href="#"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="justify-centerp-4 relative inline-flex items-center transition-transform hover:scale-105"
+        prefetch={false}
+      >
+        <div className="absolute inset-0 -z-10 animate-[ping_4s_ease-in-out_infinite] rounded-full bg-[#25D366] opacity-75" />
+        <WhatsApp />
+        <span className="sr-only">WhatsApp</span>
+      </Link>
+    </div>
+  )
+}
