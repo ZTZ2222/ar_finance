847d046e734c0264c4e1eea547c4992286deeec2
ZTZ <tzamirov@gmail.com>
2024-07-27 22:24:35 +0600

    create heading comp

---
+++ src/components/shared/heading.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { cn } from "@/lib/utils"
+
+type Props = {
+  className?: string
+  children: React.ReactNode
+}
+
+export default function Heading({ className, children }: Props) {
+  return (
+    <h2 className={cn("text-lg font-black leading-5 text-rose-750", className)}>
+      {children}
+    </h2>
+  )
+}
