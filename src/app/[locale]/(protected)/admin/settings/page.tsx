2a063360821e1d46c2a189a6f9ba01178b8cd579
ZTZ <tzamirov@gmail.com>
2024-08-07 16:10:24 +0600

    general settings layout + page + form

--- src/app/[locale]/(protected)/admin/settings/page.tsx
+++ src/app/[locale]/(protected)/admin/settings/page.tsx
@@ -1,88 +1,24 @@
-import Link from "next/link"
 import { getTranslations } from "next-intl/server"
-import { Button } from "@/components/ui/button"
 import {
   Card,
-  CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import { Checkbox } from "@/components/ui/checkbox"
-import { Input } from "@/components/ui/input"
+import GeneralSettingsForm from "./form"
 
 export default async function Settings() {
   const t = await getTranslations()
   return (
-    <div className="flex flex-1 flex-col gap-4 bg-muted/40 md:gap-8">
-      <div className="mx-auto grid w-full gap-2">
-        <h1 className="text-3xl font-semibold">
-          {t("Pages.Admin.Settings.title")}
-        </h1>
-      </div>
-      <div className="mx-auto grid w-full items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
-        <nav
-          className="grid gap-4 text-sm text-muted-foreground"
-          x-chunk="dashboard-04-chunk-0"
-        >
-          <Link href="#" className="font-semibold text-primary">
-            General
-          </Link>
-          <Link href="#">Security</Link>
-          <Link href="#">Integrations</Link>
-          <Link href="#">Support</Link>
-          <Link href="#">Organizations</Link>
-          <Link href="#">Advanced</Link>
-        </nav>
-        <div className="grid gap-6">
-          <Card x-chunk="dashboard-04-chunk-1">
-            <CardHeader>
-              <CardTitle>Store Name</CardTitle>
-              <CardDescription>
-                Used to identify your store in the marketplace.
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <form>
-                <Input placeholder="Store Name" />
-              </form>
-            </CardContent>
-            <CardFooter className="border-t px-6 py-4">
-              <Button>Save</Button>
-            </CardFooter>
-          </Card>
-          <Card x-chunk="dashboard-04-chunk-2">
-            <CardHeader>
-              <CardTitle>Plugins Directory</CardTitle>
-              <CardDescription>
-                The directory within your project, in which your plugins are
-                located.
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <form className="flex flex-col gap-4">
-                <Input
-                  placeholder="Project Name"
-                  defaultValue="/content/plugins"
-                />
-                <div className="flex items-center space-x-2">
-                  <Checkbox id="include" defaultChecked />
-                  <label
-                    htmlFor="include"
-                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                  >
-                    Allow administrators to change the directory.
-                  </label>
-                </div>
-              </form>
-            </CardContent>
-            <CardFooter className="border-t px-6 py-4">
-              <Button>Save</Button>
-            </CardFooter>
-          </Card>
-        </div>
-      </div>
-    </div>
+    <Card>
+      <CardHeader className="space-y-3.5">
+        <CardTitle>General Settings</CardTitle>
+        <CardDescription>
+          Manage the overall settings of your application. This section includes
+          options for configuring site information and appearance.
+        </CardDescription>
+      </CardHeader>
+      <GeneralSettingsForm />
+    </Card>
   )
 }
