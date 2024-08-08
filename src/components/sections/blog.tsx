34f01b2926b1bfb90ff36234621a9b0eed9fba37
ZTZ <tzamirov@gmail.com>
2024-08-09 03:48:14 +0600

    update blog card + connect blog page with db

--- src/components/sections/blog.tsx
+++ src/components/sections/blog.tsx
@@ -5,47 +5,13 @@ import BlogCard from "@/components/shared/blog-card"
 import Heading from "@/components/shared/heading"
 import SectionName from "@/components/shared/section-name"
 import Subheading from "@/components/shared/subheading"
+import { getNormalizedArticles } from "@/server/data-access-layer/article"
 import { getNormalizedSectionById } from "@/server/data-access-layer/content"
-import type { zPostRead } from "@/types/post.schema"
 
 export default async function Blog() {
   const sectionData = await getNormalizedSectionById("blog")
 
-  const posts: zPostRead[] = [
-    {
-      id: "1",
-      title:
-        "Как выбрать правильную бухгалтерскую программу для вашего бизнеса",
-      short_description:
-        "В современном мире технологий выбор подходящей бухгалтерской программы может значительно упростить вашу работу. В этой статье мы расскажем о ключевых критериях выбора и рассмотрим популярные решения на рынке.",
-      content: "Содержание статьи",
-      primaryImage: "/assets/blog/blog_image_1.jpg",
-      slug: "kak-vybrat-pravilnuyu-buhgalterskuyu-programmu-dlya-vashego-biznesa",
-      createdAt: new Date(),
-    },
-    {
-      id: "2",
-      title:
-        "Как выбрать правильную бухгалтерскую программу для вашего бизнеса",
-      short_description:
-        "В современном мире технологий выбор подходящей бухгалтерской программы может значительно упростить вашу работу. В этой статье мы расскажем о ключевых критериях выбора и рассмотрим популярные решения на рынке.",
-      content: "Содержание статьи",
-      primaryImage: "/assets/blog/blog_image_2.jpg",
-      slug: "kak-vybrat-pravilnuyu-buhgalterskuyu-programmu-dlya-vashego-biznesa",
-      createdAt: new Date(),
-    },
-    {
-      id: "3",
-      title:
-        "Как выбрать правильную бухгалтерскую программу для вашего бизнеса",
-      short_description:
-        "В современном мире технологий выбор подходящей бухгалтерской программы может значительно упростить вашу работу. В этой статье мы расскажем о ключевых критериях выбора и рассмотрим популярные решения на рынке.",
-      content: "Содержание статьи",
-      primaryImage: "/assets/blog/blog_image_3.jpg",
-      slug: "kak-vybrat-pravilnuyu-buhgalterskuyu-programmu-dlya-vashego-biznesa",
-      createdAt: new Date(),
-    },
-  ]
+  const posts = (await getNormalizedArticles()).slice(0, 3)
   return (
     <section id="blog" className="mb-[100px] space-y-[50px] md:mb-[120px]">
       <div className="container lg:flex lg:justify-between">
@@ -67,7 +33,7 @@ export default async function Blog() {
         <div className="flex w-max lg:space-x-5">
           {posts.map((post, index) => (
             <BlogCard
-              key={post.id}
+              key={post.uid}
               size="sm"
               {...post}
               className={
--- src/components/shared/blog-card.tsx
+++ src/components/shared/blog-card.tsx
@@ -1,27 +1,32 @@
+"use client"
+
 import Image from "next/image"
 import Link from "next/link"
 import { ArrowUpRight } from "lucide-react"
+import { useLocale, useTranslations } from "next-intl"
 import { cn } from "@/lib/utils"
-import { Badge } from "@/components/ui/badge"
-import type { zPostRead } from "@/types/post.schema"
+import type { NormalizedArticleRead } from "@/types/blog.schema"
 
-type BlogCardProps = zPostRead & {
+type BlogCardProps = NormalizedArticleRead & {
   size?: "sm" | "md"
   className?: string
 }
 
 export default function BlogCard({
+  uid,
   title,
-  short_description,
-  primaryImage,
-  slug,
+  content,
+  image,
   createdAt,
+  updatedAt,
   size = "md",
   className,
 }: BlogCardProps) {
+  const locale = useLocale()
+  const t = useTranslations()
   return (
     <Link
-      href={`/blog/${slug}`}
+      href={`/blog/${uid}`}
       className={cn(
         "shrink-0 space-y-8 lg:max-w-[317px] xl:max-w-[402px]",
         size === "sm" ? "max-w-[300px]" : "",
@@ -31,8 +36,8 @@ export default function BlogCard({
       {/* Image */}
       <div className={cn("relative h-[240px] overflow-hidden rounded-lg")}>
         <Image
-          src={primaryImage}
-          alt={title}
+          src={image}
+          alt={`Blog #${uid} image`}
           fill
           className="object-cover"
           sizes="(max-width: 768px) 100vw, 50vw"
@@ -42,22 +47,22 @@ export default function BlogCard({
       {/* Info */}
       <div className="flex flex-col gap-5">
         <span className="text-sm font-medium leading-[18.2px] text-rose-750">
-          {createdAt.toLocaleDateString("ru-RU", {
-            year: "numeric",
-            month: "long",
-            day: "numeric",
-          })}
+          {updatedAt?.toLocaleDateString(locale) ||
+            createdAt.toLocaleDateString(locale)}
         </span>
         <div className="flex gap-4">
-          <h4 className="text-lg font-bold leading-[21.6px] text-black">
+          <h4 className="line-clamp-2 text-lg font-bold leading-[21.6px] text-black">
             {title}
           </h4>
           <ArrowUpRight className="size-6 shrink-0" />
         </div>
-        <p className="line-clamp-3 text-gray-650">{short_description}</p>
+        <div
+          className="line-clamp-3 text-gray-650"
+          dangerouslySetInnerHTML={{ __html: content }}
+        />
 
         <div className="text-end font-semibold leading-5 text-gray-650">
-          Читать далее
+          {t("Components.Button.read-more")}
         </div>
       </div>
     </Link>
