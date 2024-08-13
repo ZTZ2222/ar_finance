ec4eaed4c2262561bd05c06ac5a2d49f1537002f
ZTZ <tzamirov@gmail.com>
2024-08-13 10:54:05 +0600

    add uploadthings remote address to next config

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,12 @@ const nextConfig = {
         port: "",
         pathname: "**",
       },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
   // output: "standalone",
